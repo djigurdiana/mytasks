e13cca08821ecb786edf7fe2fcd573dc812e6352
Диана Никулина <djigurdiana@mail.ru>
2025-10-02 01:46:54 +0500

    е1

---
+++ t1/t1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("А.С. Пушкин");
+                Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Узник");
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Сижу за решеткой в темнице сырой.");
+            Console.WriteLine("Вскормленный в неволе орел молодой.");
+            Console.WriteLine("Мой грустный товарищ, махая крылом,");
+            Console.WriteLine("Кровавую пищу клюет под окном.");
+
+
+        }
+    }
+}
