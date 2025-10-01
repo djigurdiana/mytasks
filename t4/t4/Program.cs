59587ff5eae716d9327cc7f21aec316eefcd63e4
Диана Никулина <djigurdiana@mail.ru>
2025-10-02 01:40:28 +0500

    t4

---
+++ t4/t4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+
+            var x = double.Parse(Console.ReadLine());
+            var y = F(x);
+
+            double baseNumber = Math.Tan(x);
+            double baseNumber2 = Math.Cos(x);
+
+            Console.WriteLine("y = "+ y);
+        }
+        static double F(double x) => Math.Abs(x) + 2 * Math.Sin(Math.Pow(Math.Tan(x), 2) + 4)/ 5.5 * x;
+    }
+}
